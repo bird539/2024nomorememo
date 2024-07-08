f017f69e3dfe9b9089db7e961ec897c8c9dab7e0
Ju Nan <35447697+bird539@users.noreply.github.com>
2024-07-09 03:55:55 +0900

    Add files via upload

--- nomorememo2024/LocalJS.js
+++ nomorememo2024/LocalJS.js
@@ -1,7 +1,118 @@
 const mainDiv = document.querySelector(".main");
 const mainHtml = document.querySelector('html');
 mainHtml.style.height = '100%';
+function showHide(event) {
+    const strArray = event.target.className.split('_');
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i].includes("showHide")) {
+            const targetName = strArray[i].split(":")[1];
+            const target = document.querySelector(`.${targetName}`);
+            target.style.display = target.style.display != "none" ? "none" : strArray[i].split(":")[2];
+        }
+    }
+}
 
+const date = new Date();
+
+//console.log(date);
+//console.log(date.setDate(20));
+//console.log(date);
+
+function timeSomthing(time){
+    const befo = new Date("Tue Jul 09 2024 03:25:25 GMT+0900 (한국 표준시)");
+    const befoT = {
+        year:befo.getFullYear(),
+        month:befo.getMonth(),
+        date:befo.getDate(),
+        hours:befo.getHours(),
+        minuts:befo.getMinutes(),
+        seconds:befo.getSeconds(),
+        day:befo.getDay() //요일
+    }
+
+    const plsT = {
+        year : 0,
+        month: 0,
+        date  : 0,
+        hours: 0,
+        minuts: 80,
+        seconds: 300
+    };
+
+    let tem = 0;
+    if(plsT.seconds > 60){
+        tem += plsT.seconds;
+        plsT.seconds = tem % 60; plsT.minuts += parseInt(tem / 60); 
+    }
+    if(plsT.minuts > 60){ 
+        tem =0; tem += plsT.minuts;
+        plsT.minuts = tem % 60; plsT.hours += parseInt(tem / 60); }
+    if(plsT.hours > 24){ 
+        tem =0; tem += plsT.hours;
+        plsT.hours = tem % 60; plsT.date += parseInt(tem / 24); }
+
+    const now = new Date();
+    const nowT = {
+        year:now.getFullYear(),
+        month:now.getMonth(),
+        date:now.getDate(),
+        hours:now.getHours(),
+        minuts:now.getMinutes(),
+        seconds:now.getSeconds(),
+        day:now.getDay() //요일
+    }
+
+    
+    let endT = {
+        year : 0,
+        month: 0,
+        day  : 0,
+        hours: 0,
+        minuts: 0,
+        seconds: 0
+    };
+    const after = new Date();
+    console.log("after", after);
+
+    if(plsT.year != 0){ endT.year = befoT.year + plsT.year; after.setFullYear(endT.year); };
+    if(plsT.month != 0){ endT.month = befoT.month + plsT.month; after.setMonth(endT.month); };
+    if(plsT.date != 0){ endT.date = befoT.date + plsT.date; after.setDate(endT.date); };
+    if(plsT.hours != 0){ endT.hours = befoT.hours + plsT.hours; after.setHours(endT.hours); };
+    if(plsT.minuts != 0){ endT.minuts = befoT.minuts + plsT.minuts; after.setMinutes(endT.minuts); };
+    if(plsT.seconds != 0){ endT.seconds = befoT.seconds + plsT.seconds; after.setSeconds(endT.seconds); };
+
+    console.log(befoT.hours, plsT.hours);
+    console.log(befo);
+    console.log(now);
+    console.log(after);
+    console.log(endT);
+    console.log(`${endT.hours}:${endT.minuts}=${nowT.minuts}+${plsT.minuts}` );
+    console.log(`rest = ${endT.minuts - nowT.minuts}` );
+
+    /*
+    let time = {
+        madeT : Tue Jul 09 2024 01:52:40 GMT+0900 (한국 표준시)
+        plsT : {year,month,day,hours,minuts,seconds},
+        endT : null <- made + pls
+    }
+    return time = {
+        made : Tue Jul 09 2024 01:52:40 GMT+0900 (한국 표준시)
+        madeTime : {year,month,day,hours,minuts,seconds},
+        pluseTime : {year,month,day,hours,minuts,seconds},
+        endTime : made + pls
+        repeat : 
+
+        text : "~후 까지"
+    }
+    */
+    
+}
+let time = {
+    madeT : date,
+    plsT : null,//{year,month,day,hours,minuts,seconds},
+    endT : null,//null <- made + pls
+}
+timeSomthing(time);
 class htmlRemote {
     html_backgroundColor = "";
     html_ligthDarkMode = "";
@@ -80,16 +191,7 @@ class Tab_Memo {
 
 }
 
-function showHide(event) {
-    const strArray = event.target.className.split('_');
-    for (let i = 0; i < strArray.length; i++) {
-        if (strArray[i].includes("showHide")) {
-            const targetName = strArray[i].split(":")[1];
-            const target = document.querySelector(`.${targetName}`);
-            target.style.display = target.style.display != "none" ? "none" : strArray[i].split(":")[2];
-        }
-    }
-}
+
 class htmlRemoteElement{
     div = null;
     button = null;
@@ -133,128 +235,201 @@ class htmlRemoteElement{
         this.summary = document.createElement("summary");
         this.htmlRemoteDiv = null;
     }
-
     setValue(db) {
-        this.db.lastPageShow = db.lastPageShow;
-        this.db.title = db.title;
-
-        this.db.fontSize = db.fontSize;
-        this.db.fontWeight = db.fontWeight;
-        this.db.fontFamily = db.fontFamily;
-        this.db.fontStyle = db.fontType;
-
-        this.db.fontColor = db.fontColor;
-        this.db.backgroundColor = db.backgroundColor;
-
-        this.db.htmlBackgroundColor = db.htmlBackgroundColor;
-        this.db.lightDarkMode = db.lightDarkMode;
-        this.db.language = db.language;
-
-        this.db.newInfo = db.newInfo;
-        this.db.schedule = db.schedule;
+            this.db.lastPageShow = db.lastPageShow;
+            this.db.title = db.title;
     
-    //쓰레기통 - 윈도우     (삭제일key, 윈도우 제목, 탭 제목들, 탭 정보들 )
-        this.db.trashWindow = db.trashWindow;        //삭제일 + 윈도우div(tap디테일(탭 글자들)) + 복구 btn
-    //쓰레기통 - 탭들       (삭제일key, 탭 제목들, 탭 정보들 )
-        this.db.trashTab = db.trashTab;           //삭제일 + tap디테일(탭 글자들))          + 복구 btn
-    //쓰레기통 - 탭 정보들  (삭제일key, 탭 정보들 )               
-        this.db.trashTabText= db.trashTabText;//삭제일  + 탭 글자들           + 복구 btn
-
-        //make basic
-        this.button.style.backgroundColor = "transparent";
-        this.input.style.backgroundColor = "transparent";
-        this.div.style.backgroundColor = "transparent";
-        this.select.style.backgroundColor = "transparent";
-        this.details.style.backgroundColor = "transparent";
-        this.summary.style.backgroundColor = "transparent";
-        this.option.style.backgroundColor = this.db.backgroundColor;
-
-        this.select.style.border = "none";
-        this.button.style.border = "none";
-        this.input.style.border = "none";
-
-        this.select.style.padding = "0";
-        this.button.style.padding = "0";
-        this.input.style.padding = "0";
-
-        this.select.style.marginRight = "10px";
-        this.button.style.marginRight = "10px";
-
-        this.select.style.height = "40px";
-        this.button.style.height = "40px";
-        this.input.style.height = "40px";
-
-        this.select.style.fontFamily = this.db.fontFamily;
-        this.select.style.fontSize = `${this.db.fontSize}pt`;
-        this.select.style.fontWeight = this.db.fontThick;
-        this.select.style.fontStyle = this.db.fontStyle;
-        this.select.style.color = this.db.fontColor;
+            this.db.fontSize = db.fontSize;
+            this.db.fontWeight = db.fontWeight;
+            this.db.fontFamily = db.fontFamily;
+            this.db.fontStyle = db.fontType;
+    
+            this.db.fontColor = db.fontColor;
+            this.db.backgroundColor = db.backgroundColor;
+    
+            this.db.htmlBackgroundColor = db.htmlBackgroundColor;
+            this.db.lightDarkMode = db.lightDarkMode;
+            this.db.language = db.language;
+    
+            this.db.newInfo = db.newInfo;
+            this.db.schedule = db.schedule;
         
-        this.option.style.fontFamily = this.db.fontFamily;
-        this.option.style.fontSize = `${this.db.fontSize}pt`;
-        this.option.style.fontWeight = this.db.fontThick;
-        this.option.style.fontStyle = this.db.fontStyle;
-        this.option.style.color = this.db.fontColor;
-
-        this.button.style.fontFamily = this.db.fontFamily;
-        this.button.style.fontSize = `${this.db.fontSize}pt`;
-        this.button.style.fontWeight = this.db.fontThick;
-        this.button.style.fontStyle = this.db.fontStyle;
-        this.button.style.color = this.db.fontColor;
-        
-        this.input.style.fontFamily = this.db.fontFamily;
-        this.input.style.fontSize = `${this.db.fontSize}pt`;
-        this.input.style.fontWeight = this.db.fontThick;
-        this.input.style.fontStyle = this.db.fontStyle;
-        this.input.style.color = this.db.fontColor;
-
-        this.div.style.fontFamily = this.db.fontFamily;
-        this.div.style.fontSize = `${this.db.fontSize}pt`;
-        this.div.style.fontWeight = this.db.fontThick;
-        this.div.style.fontStyle = this.db.fontStyle;
-        this.div.style.color = this.db.fontColor;
-
-        this.details.style.fontFamily = this.db.fontFamily;
-        this.details.style.fontSize = `${this.db.fontSize}pt`;
-        this.details.style.fontWeight = this.db.fontThick;
-        this.details.style.fontStyle = this.db.fontStyle;
-        this.details.style.color = this.db.fontColor;
-
-        this.summary.style.fontFamily = this.db.fontFamily;
-        this.summary.style.fontSize = `${this.db.fontSize}pt`;
-        this.summary.style.fontWeight = this.db.fontThick;
-        this.summary.style.fontStyle = this.db.fontStyle;
-        this.summary.style.color = this.db.fontColor;
+        //쓰레기통 - 윈도우     (삭제일key, 윈도우 제목, 탭 제목들, 탭 정보들 )
+            this.db.trashWindow = db.trashWindow;        //삭제일 + 윈도우div(tap디테일(탭 글자들)) + 복구 btn
+        //쓰레기통 - 탭들       (삭제일key, 탭 제목들, 탭 정보들 )
+            this.db.trashTab = db.trashTab;           //삭제일 + tap디테일(탭 글자들))          + 복구 btn
+        //쓰레기통 - 탭 정보들  (삭제일key, 탭 정보들 )               
+            this.db.trashTabText= db.trashTabText;//삭제일  + 탭 글자들           + 복구 btn
+    
+            //make basic
+            this.button.style.backgroundColor = "transparent";
+            this.input.style.backgroundColor = "transparent";
+            this.div.style.backgroundColor = "transparent";
+            this.select.style.backgroundColor = "transparent";
+            this.details.style.backgroundColor = "transparent";
+            this.summary.style.backgroundColor = "transparent";
+            this.option.style.backgroundColor = this.db.backgroundColor;
+    
+            this.select.style.border = "none";
+            this.button.style.border = "none";
+            this.input.style.border = "none";
+    
+            this.select.style.padding = "0";
+            this.button.style.padding = "0";
+            this.input.style.padding = "0";
+    
+            this.select.style.marginRight = "10px";
+            this.button.style.marginRight = "10px";
+    
+            this.select.style.height = "40px";
+            this.button.style.height = "40px";
+            this.input.style.height = "40px";
+    
+            this.select.style.fontFamily = this.db.fontFamily;
+            this.select.style.fontSize = `${this.db.fontSize}pt`;
+            this.select.style.fontWeight = this.db.fontThick;
+            this.select.style.fontStyle = this.db.fontStyle;
+            this.select.style.color = this.db.fontColor;
+            
+            this.option.style.fontFamily = this.db.fontFamily;
+            this.option.style.fontSize = `${this.db.fontSize}pt`;
+            this.option.style.fontWeight = this.db.fontThick;
+            this.option.style.fontStyle = this.db.fontStyle;
+            this.option.style.color = this.db.fontColor;
+    
+            this.button.style.fontFamily = this.db.fontFamily;
+            this.button.style.fontSize = `${this.db.fontSize}pt`;
+            this.button.style.fontWeight = this.db.fontThick;
+            this.button.style.fontStyle = this.db.fontStyle;
+            this.button.style.color = this.db.fontColor;
+            
+            this.input.style.fontFamily = this.db.fontFamily;
+            this.input.style.fontSize = `${this.db.fontSize}pt`;
+            this.input.style.fontWeight = this.db.fontThick;
+            this.input.style.fontStyle = this.db.fontStyle;
+            this.input.style.color = this.db.fontColor;
+    
+            this.div.style.fontFamily = this.db.fontFamily;
+            this.div.style.fontSize = `${this.db.fontSize}pt`;
+            this.div.style.fontWeight = this.db.fontThick;
+            this.div.style.fontStyle = this.db.fontStyle;
+            this.div.style.color = this.db.fontColor;
+    
+            this.details.style.fontFamily = this.db.fontFamily;
+            this.details.style.fontSize = `${this.db.fontSize}pt`;
+            this.details.style.fontWeight = this.db.fontThick;
+            this.details.style.fontStyle = this.db.fontStyle;
+            this.details.style.color = this.db.fontColor;
+    
+            this.summary.style.fontFamily = this.db.fontFamily;
+            this.summary.style.fontSize = `${this.db.fontSize}pt`;
+            this.summary.style.fontWeight = this.db.fontThick;
+            this.summary.style.fontStyle = this.db.fontStyle;
+            this.summary.style.color = this.db.fontColor;
     }
+    //<-- set value
+
+    //function event -->
+    function_selectEvent(event){
+        const select = event.target;
+        //`w${this.db.index}editSelectDiv_w${this.db.index}editBookDiv`;
+        const targetClassName = select.className.split("_")[1].split(":");
+        const target = document.querySelector(`.${targetClassName[0]}`);
+        for(let i=0; i<target.childNodes.length; i++){
+            target.childNodes[i].style.display = "none";
+        }
+        target.childNodes[select.selectedIndex].style.display = targetClassName[1];
+        //select.dispatchEvent(new Event('change'));
+    }
+
+    function_nextBefoEvent(event){
+        const btn = event.target;
+        const select = btn.parentNode.previousSibling;
+        const newIndex = btn.innerText == ">" ? +1 : -1; 
+        let index = select.selectedIndex + newIndex;
+        let lastIndex = select.childNodes.length-1;
+        if(index < 0){ index = lastIndex }else if(index > lastIndex){ index = 0 }
+        select.selectedIndex = index;
+        select.dispatchEvent(new Event('change'));
+    }
+
+    newRecentInfoMake(newInfo){
+        const newInfoDiv = this.div.cloneNode(true);
+        newInfoDiv.style.display = "flex";
+        newInfoDiv.style.flexGrow = "1";
+
+        const dateDiv = newInfoDiv.cloneNode(true);
+        const befoTime =new Date(newInfo.date);
+        const afterTime = `${befoTime.getMonth()}/${befoTime.getDate()}/${befoTime.getHours()}:${befoTime.getMinutes()}`
+        dateDiv.innerText = afterTime;
+
+        const windowDiv = newInfoDiv.cloneNode(true);
+        windowDiv.innerText = newInfo.window;
+
+        const textDiv = newInfoDiv.cloneNode(true);
+        textDiv.innerText = newInfo.text;
+
+        newInfoDiv.appendChild(dateDiv);
+        newInfoDiv.appendChild(windowDiv);
+        newInfoDiv.appendChild(textDiv);
+
+        return newInfoDiv;
+    }
+    newRestWorkInfoMake(newInfo){
+        const newInfoDiv = this.div.cloneNode(true);
+        newInfoDiv.style.display = "flex";
+        newInfoDiv.style.flexGrow = "1";
+
+        const endTimeDiv = newInfoDiv.cloneNode(true);
+        const retTimeDiv = newInfoDiv.cloneNode(true);
+        const endTime =new Date(newInfo.endTime);
+        const now = new Date();
+
+        console.log("end",endTime);
+        console.log("end info",newInfo.endTime);
+        console.log("now",now);
+        
+        endTimeDiv.innerText = `${endTime.getMonth()}/${endTime.getDate()}/${endTime.getHours()}:${endTime.getMinutes()}`
+        retTimeDiv.innerText = `${endTime.getMonth() - now.getMonth()}/${endTime.getDate() - now.getDate()}/${endTime.getHours() - now.getHours()}:${endTime.getMinutes() - now.getMinutes()}`
+        
+        const windowDiv = newInfoDiv.cloneNode(true);
+        windowDiv.innerText = newInfo.window;
+
+        const textDiv = newInfoDiv.cloneNode(true);
+        textDiv.innerText = newInfo.text;
+
+        newInfoDiv.appendChild(endTimeDiv);
+        newInfoDiv.appendChild(retTimeDiv);
+        newInfoDiv.appendChild(windowDiv);
+        newInfoDiv.appendChild(textDiv);
+
+        return newInfoDiv;
+    }
+    //<-- function event
 
     recentPlsPage(){
         const pageDiv = this.div.cloneNode(true);
         pageDiv.style.width = "flex";
-        console.log(this.db.newInfo);
         for(let i=0; i<this.db.newInfo.length; i++){
-            const newInfoDiv = this.div.cloneNode(true);
-            newInfoDiv.style.display = "flex";
-            newInfoDiv.style.flexGrow = "1";
-
-            const dateDiv = newInfoDiv.cloneNode(true);
-            const befoTime =new Date(this.db.newInfo[i].date);
-            const afterTime = `${befoTime.getMonth()}/${befoTime.getDate()}/${befoTime.getHours()}:${befoTime.getMinutes()}`
-            dateDiv.innerText = afterTime;
-
-            const windowDiv = newInfoDiv.cloneNode(true);
-            windowDiv.innerText = this.db.newInfo[i].window;
-
-            const textDiv = newInfoDiv.cloneNode(true);
-            textDiv.innerText = this.db.newInfo[i].text;
-
-            newInfoDiv.appendChild(dateDiv);
-            newInfoDiv.appendChild(windowDiv);
-            newInfoDiv.appendChild(textDiv);
-
-            pageDiv.appendChild(newInfoDiv);
+            pageDiv.appendChild(this.newRecentInfoMake(this.db.newInfo[i]));
         }
+        pageDiv.className = "recentPlsPage";
         return pageDiv;
     }
+    restWorkPage(){
+        const pageDiv = this.div.cloneNode(true);
+        pageDiv.style.width = "flex";
+        for(let i=0; i<this.db.newInfo.length; i++){
+            this.db.newInfo[i].text = "work work";
+            console.log(this.db.schedule);
+            //pageDiv.appendChild(this.newRestWorkInfoMake(this.db.schedule[i]));
+        }
+        //임시 숨기기
+        pageDiv.className = "restWorkPage";
+        pageDiv.style.display = "none";
+        return pageDiv;
+    }
+
 
     setElementEditBook(){
         const bodyDiv = this.div.cloneNode(true);
@@ -265,7 +440,11 @@ class htmlRemoteElement{
         bodyDiv.style.float = "right";
         bodyDiv.className = "htmlRemoteBody";
 
+        //`w${this.db.index}editSelectDiv_w${this.db.index}editBookDiv`;
         const pageSelect = this.select.cloneNode(true);
+        pageSelect.className = "htmlEditSelectDiv_htmpPagesDiv:flex";
+        //function_selectEvent
+        pageSelect.addEventListener("change",this.function_selectEvent);
         pageSelect.style.width = "100%"
         let pageName = ["최근 추가","남은 일정", "쓰레기통","remote 꾸미기",];
         for (let i = 0; i < pageName.length; i++) {
@@ -291,9 +470,11 @@ class htmlRemoteElement{
         const befo = next.cloneNode(true);
         befo.innerText = "<";
         next.innerText = ">";
-
+        befo.addEventListener("click", this.function_nextBefoEvent);
+        next.addEventListener("click", this.function_nextBefoEvent);
+        
         const pagesDiv = this.div.cloneNode(true);
-        pagesDiv.className = "pagesDiv";
+        pagesDiv.className = "htmpPagesDiv";
         pagesDiv.style.display = "flex";
         pagesDiv.style.float = "right";
         pagesDiv.style.flexDirection = "row";
@@ -303,6 +484,7 @@ class htmlRemoteElement{
         BookDiv.appendChild(befo);
 
         pagesDiv.appendChild(this.recentPlsPage());
+        pagesDiv.appendChild(this.restWorkPage());
         
         BookDiv.appendChild(pagesDiv);
         BookDiv.appendChild(next);
@@ -834,7 +1016,8 @@ let remoteDb = {
     //html 설정
     htmlBackgroundColor: "#95C2FE", lightDarkMode: false, language: 0,
     //최근 항목(date, text) / 일정모음(마감시간,남은시간,텍스트)
-    newInfo: [{date:today,window:"win1",text:"text1"},], schedule:[],
+    newInfo: [{date:today,window:"win1",text:"text1"},{date:today,window:"win2win2",text:"text2text2"},], 
+    schedule:[{endTime:today, text:"이것 저것 구매하기 목록"}],
     
     //쓰레기통 - 윈도우     (삭제일key, 윈도우 제목, 탭 제목들, 탭 정보들 )
     trashWindow: [],        //삭제일 + 윈도우div(tap디테일(탭 글자들)) + 복구 btn
